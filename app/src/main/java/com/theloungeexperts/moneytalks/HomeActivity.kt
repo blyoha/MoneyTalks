19eb492f3cc068a8336ea7ff6d4c2c1cb6483aa3
neop-haw <filling_box@yahoo.com>
2021-11-28 12:46:18 +0300

    Changed HomeActivity.kt

--- app/src/main/java/com/theloungeexperts/moneytalks/HomeActivity.kt
+++ app/src/main/java/com/theloungeexperts/moneytalks/HomeActivity.kt
@@ -7,18 +7,10 @@ import com.theloungeexperts.moneytalks.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     lateinit var binding: ActivityHomeBinding
-    private val chatPreviewAdapter = ChatPreviewAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
-
-    private fun initChatMenu() {
-        binding.apply {
-            chatList.layoutManager = LinearLayoutManager(this@HomeActivity)
-            chatList.adapter = chatPreviewAdapter
-        }
     }
 }
